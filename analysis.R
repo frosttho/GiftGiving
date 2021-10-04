4aa29ba511c8ef82517423ef74d9ed42d95785dc
Thomas <44763561+frosttho@users.noreply.github.com>
2021-10-04 14:43:51 +0200

    Data import
    Import data and a first simple descriptive diagram with ggplot

---
+++ analysis.R
@@ -0,0 +1,51 @@
+#######################################
+#                                     #
+#            gift giving              #
+#          survey analysis            #
+#                                     #
+#            Thomas FROST             #
+#           Antonia GERKEN            #
+#           Anita LINDBERG            #
+#             Diane OWIN              #
+#                                     #
+#       emlyon business school        #
+#                                     #
+#       https://lyonexchange.fr       #
+#     gift-giving@lyonexchange.fr     #
+#                                     #
+#######################################
+
+
+
+# 00 - Setup #############################
+
+setwd("~/Studium/Master/Module/5A2M21 Advanced Topics in Digital Marketing/Gruppenarbeit")
+
+library(tidyverse)
+
+
+
+# 01 - Read data #########################
+
+surveydata <- read_csv(file = "results-survey282822_20211004-1405-codes.csv")
+
+# delete incomplete answers
+surveydata <- filter(surveydata, !is.na(submitdate))
+
+
+# 02 - Descriptive analysis ##############
+
+# Format yes/no answer as Factor Variable
+surveydata$G01Q04 <- as.factor(surveydata$G01Q04)
+
+# Show percentage of the two answers
+summary(surveydata$G01Q04)/nrow(surveydata)
+
+# plot a simple bar diagram
+plot <- ggplot(surveydata) +
+  geom_bar(aes(x = G01Q04)) +
+  ylab("Number of respondents") +
+  xlab("Answer") +
+  ggtitle("G01Q04 Familiarity of problem situation") +
+  coord_flip()
+plot
