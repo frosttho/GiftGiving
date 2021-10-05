b72c738c3d0429fdabdbae1254c58e8243ab13d9
Thomas <44763561+frosttho@users.noreply.github.com>
2021-10-05 12:59:26 +0200

    Gift item plot
    Added draft for a gift item boxplot

--- analysis.R
+++ analysis.R
@@ -22,7 +22,6 @@
 library(tidyverse)
 
 
-
 # 01 - Read data #########################
 
 # surveydata <- read_csv(file = "results-survey282822_20211004-1449.csv")
@@ -32,10 +31,34 @@ surveydata <- read_csv(file = "results-survey282822_20211005-1218.csv")
 surveydata <- filter(surveydata, !is.na(submitdate))
 
 
+
+
+
 # 02 - Descriptive analysis ##############
+
+
+# different gift items (G02Q07)
+items <- c('Seads', 'Spices', 'Chocolate, Pralines', 'Coffee / Tea', 'Food / sweats in general', 'Specialties from the whole country / EU / world', 'Flowers', 'Candles', 'Cosmetics', 'Alcohol')
+labels <- c('Seads', 'Spices', 'Chocolate,\nPralines', 'Coffee /\nTea', 'Food / sweats\nin general', 'Specialties from\nthe whole country /\nEU / world', 'Flowers', 'Candles', 'Cosmetics', 'Alcohol')
+
+giftitems <- reshape(surveydata[15:24], times = items, timevar = "gift", direction = "long", varying = 1:10, v.names = "rating")
+rownames(giftitems) <- 1:nrow(giftitems)
+giftitems$gift <- as.factor(giftitems$gift)
+
+itemsplot <- ggplot(giftitems) +
+  theme_classic() +
+  geom_boxplot(aes(x = gift, y = rating)) +
+  ylab("Rating (1 to 5)") +
+  xlab("") +
+  ggtitle("G02Q07 Popularity of gift box items") +
+  scale_x_discrete(labels= labels)
+itemsplot
+
+
 
 # Format yes/no answer as Factor Variable
 surveydata$G01Q04 <- as.factor(surveydata$G01Q04)
+
 
 # Show percentage of the two answers
 summary(surveydata$G01Q04)/nrow(surveydata)
